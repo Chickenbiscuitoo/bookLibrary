3799fb36273a6a478e135984f2434963a3b715ee
Chickenbiscuitoo <chickenbiscuito.dev@gmail.com>
2022-01-06 21:19:21 +0100

    v1.0

--- code.js
+++ code.js
@@ -1,49 +1,66 @@
-let myLibrary = [];
-
-function Book(author, title, pages, read) {
-    this.author = author;
-    this.title = title;
-    this.pages = pages;
-    this.read = read;
-}
-
-function addBookToLibrary(author, title, pages, read) {
-    let newBook = new Book(author, title, pages, read);
-    myLibrary.push(newBook);
-    console.log(myLibrary)
-}
-
-function displayBooks() {
-    const divLibrary = document.querySelector('#divLibrary');
-    
-    for (let i = 0; i < myLibrary.length; i++) {
-        let divBook = document.createElement('div');
-        divLibrary.appendChild(divBook).className = 'divBook';
-
-        let bookTitle = myLibrary[i].title
-        divBook.innerHTML = bookTitle;
+// BOOK CLASS
+class Book {
+    constructor(title, author, pages) {
+        this.title = title,
+        this.author = author,
+        this.pages = pages
     }
 }
 
-function showForm() {
-    let form = document.getElementById("form");
-    if (!form.style.display) {
-        form.style.display = "none";
+// UI CLASS
+class UI {
+    static displayBooks() {
+        const storedBooks = [
+            {
+                title: 'Harry Potter',
+                author: 'J. K Rowling',
+                pages: '896'
+            },
+            {
+                title: 'Fight Club',
+                author: 'John Van Dude',
+                pages: '658'
+            },
+            {
+                title: 'Berserk',
+                author: 'Kentaro Miura',
+                pages: '1503'
+            }
+        ]
+        const books = storedBooks;
+
+        books.forEach((book) => {
+            UI.addBookToList(book);
+        })
     }
-    if (form.style.display === "none") {
-        form.style.display = "block";
-    } else {
-        form.style.display = "none";
+
+    static addBookToList(book) {
+        const bookListTitle = document.getElementById('title');
+        const bookListAuthor = document.getElementById('author');
+        const bookListPages = document.getElementById('pages');
+
+        const bookTitle = document.createElement('p');
+        const bookAuthor = document.createElement('p');
+        const bookPages = document.createElement('p');
+
+        bookTitle.textContent = `${book.title}`;
+        bookAuthor.textContent = `${book.author}`;
+        bookPages.textContent = `${book.pages}`;
+
+        bookListTitle.appendChild(bookTitle);
+        bookListAuthor.appendChild(bookAuthor);
+        bookListPages.appendChild(bookPages);
     }
 }
 
-document.getElementById('btnSubmit').addEventListener('click', (event) => {
-    let inputAuthor = document.getElementById('inputAuthor').value;
-    let inputTitle = document.getElementById('inputTitle').value;
-    let inputPages = document.getElementById('inputPages').value;
-    let inputRead = document.getElementById('inputRead').value;
+// STORE CLASS
 
-    addBookToLibrary(inputAuthor, inputTitle, inputPages, inputRead);
-    displayBooks();
-    event.preventDefault();
-});
+// EVENT: DISPLAY BOOKS
+const btnSubmit = document.getElementById('submit-form-btn')
+btnSubmit.addEventListener('click', () => {
+    console.log('ey')
+})
+
+// EVENT: ADD A BOOK
+
+// EVENT: REMOVE A BOOK
